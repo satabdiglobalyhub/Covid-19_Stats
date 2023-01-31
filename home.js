ccb69c66c0a704e29b647ae8585784d95727f8c9
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-01-31 13:43:39 +0545

    feat:countriesLists

--- home.js
+++ home.js
@@ -1,41 +1,65 @@
-async function getCovidapi() {
-  const jsondata = await fetch("https://api.covid19api.com/country/nepal");
-  const jsdata = await jsondata.json();
-  console.log(jsdata);
-  const Todaydata = jsdata.Global;
+async function getCountryListapi() {
+  const jsondataCountries = await fetch("https://api.covid19api.com/countries");
+  const jsdataCountries = await jsondataCountries.json();
 
-  //   let countryList = jsdata.forEach((country)=>{
-  // console.log(country.Country);
-  //   })
+  let countriesDropDown = document.getElementById("countriesDropDown");
 
-  // const Todaydata = jsdata[jsdata.length - 1];
-  //   const jsondata = await fetch("https://api.covid19api.com/countries");
-  //   const jsdata= await jsondata.json();
-  //   let countryList = jsdata.forEach((country)=>{
-  // console.log(country.Country);
-  //   })
-  // console.log(`Total Cases are ${Todaydata.Confirmed}`);
-  // let conformedCases = document.querySelector("#todayConfirmed");
-  // let conformedNum = Todaydata.Confirmed;
-  // var conformedNum1 = conformedNum.toLocaleString("hi-IN");
-  // conformedCases.append(conformedNum1);
+  jsdataCountries.forEach((item) => {
+    let option = document.createElement("option");
+    option.setAttribute("value", item.Slug);
 
-  // // console.log(`Recovered Cases are ${Todaydata.Recovered}`);
-  // let conformedRecovered = document.querySelector("#todayRecovered");
-  // let recoveredNum = Todaydata.Recovered;
-  // var recoveredNum1 = recoveredNum.toLocaleString("hi-IN");
-  // conformedRecovered.append(recoveredNum1);
+    let optionText = document.createTextNode(item.Country);
+    option.appendChild(optionText);
 
-  // // console.log(`Total Deaths are ${Todaydata.Deaths}`);
-  // let conformedDeaths = document.querySelector("#totalDeaths");
-  // let deathNum = Todaydata.Deaths;
-  // var deathNum1 = deathNum.toLocaleString("hi-IN");
-  // conformedDeaths.append(deathNum1);
-
-  // // console.log(`Last updated time: ${date.toLocaleDateString('en-US')}`);
-  // let todaysDate = document.querySelector("#bodyUpdated");
-  // const date = new Date(Todaydata.Date);
-  // todaysDate.append(date.toLocaleDateString("en-US"));
+    countriesDropDown.appendChild(option);
+  });
 }
 
+async function handleCountryChange(selected) {
+  const selectedUrl = `https://api.covid19api.com/country/${selected.value}`;
+  console.log(selectedUrl);
+}
+
+async function getCovidapi() {
+  const jsonCoviddata = await fetch("https://api.covid19api.com/summary");
+  const jsCoviddata = await jsonCoviddata.json();
+  let todaydata = jsCoviddata.Global;
+
+  let todayConformedCases = document.querySelector("#todayConfirmed");
+  let todayConformedNum = todaydata.NewConfirmed;
+  var todayConformedNum1 = todayConformedNum.toLocaleString("hi-IN");
+  todayConformedCases.append(todayConformedNum1);
+
+  let todayRecoveredCases = document.querySelector("#todayRecovered");
+  let todayRecoveredNum = todaydata.NewRecovered;
+  var todayRecoveredNum1 = todayRecoveredNum.toLocaleString("hi-IN");
+  todayRecoveredCases.append(todayRecoveredNum1);
+
+  let todayDeathCases = document.querySelector("#todayDeaths");
+  let todayDeathNum = todaydata.NewDeaths;
+  var todayDeathNum1 = todayDeathNum.toLocaleString("hi-IN");
+  todayDeathCases.append(todayDeathNum1);
+
+  let totalConformedCases = document.querySelector("#totalConfirmed");
+  let totalConformedNum = todaydata.TotalConfirmed;
+  var totalConformedNum1 = totalConformedNum.toLocaleString("hi-IN");
+  totalConformedCases.append(totalConformedNum1);
+
+  let totalRecoveredCases = document.querySelector("#totalRecovered");
+  let totalRecoveredNum = todaydata.TotalRecovered;
+  var totalRecoveredNum1 = totalRecoveredNum.toLocaleString("hi-IN");
+  totalRecoveredCases.append(totalRecoveredNum1);
+
+  let totalDeathCases = document.querySelector("#totalDeaths");
+  let totalDeathNum = todaydata.TotalDeaths;
+  var totalDeathNum1 = totalDeathNum.toLocaleString("hi-IN");
+  totalDeathCases.append(totalDeathNum1);
+
+  let todayDate = document.querySelector("#todayDate");
+  const todayDateNum = new Date(todaydata.Date);
+  var todayDateNum1 = todayDateNum.toLocaleDateString("en-US");
+  todayDate.append(todayDateNum1);
+}
+
+getCountryListapi();
 getCovidapi();
